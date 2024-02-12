47cb4f30bcf1888005df48323a1d75b551cd4b58
ChikovMF <ia-matvey@mail.ru>
2024-02-12 15:33:10 +0300

    Add several algorithms

--- SHOLF.Console/Program.cs
+++ SHOLF.Console/Program.cs
@@ -1,16 +1,24 @@
 ﻿using SHOLF.Library;
 using System.Security.Cryptography;
 
-using (var stream = new FileStream(@$"C:\l.bin", FileMode.Open, FileAccess.Read))
+using (var stream = new FileStream(@$"C:\l2.bin", FileMode.Open, FileAccess.Read))
 {
     var alg = SHA256.Create();
     var h = new Hasher();
-    var strs = h.GetHash(stream, HashingAlgorithm.MD5, 1);
+    
+    Console.WriteLine("File name: " + stream.Name + ". File size: " + stream.Length + " byte.");
 
-    int i = 1;
-    foreach (var str in strs)
+    foreach(HashingAlgorithm algorithm in Enum.GetValues(typeof(HashingAlgorithm)))
     {
-        Console.WriteLine(i + ") " + str);
-        i++;
+        var strs = h.GetHash(stream, algorithm, 3);
+
+        int i = 1;
+        Console.WriteLine(algorithm.ToString() + ":");
+        foreach (var str in strs)
+        {
+            Console.WriteLine(i + ") " + str);
+            i++;
+        }
+        Console.WriteLine();
     }
 }
